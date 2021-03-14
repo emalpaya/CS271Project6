1595726beee499d80dd1d5540381a038c577df51
Eva Malpaya <malpaya.eva@gmail.com>
2021-03-14 13:02:48 -0700

    3/13 EOD progress

--- Proj6_malpayae.asm
+++ Proj6_malpayae.asm
@@ -16,16 +16,19 @@ INCLUDE Irvine32.inc
 ; 
 ; preconditions:	
 ; postconditions:	
-; receives:			prompt (ref), count (val)
-; returns:			userInput (ref), bytesRead (ref)
+; receives:			prompt, userInput, count, bytesRead
+; returns:			userInput, bytesRead updated
 ;--------------------------------------
-mGetString MACRO prompt, count
-
+mGetString MACRO prompt, userInput, count, bytesRead
 	; Display a prompt
+	mDisplayString prompt
 
 	; Get user's keyboard input into a memory location
-
-	;
+	MOV		EDX, userInput
+	MOV		ECX, count
+	CALL	ReadString
+	MOV		userInput, EDX
+	MOV		bytesRead, EAX
 
 ENDM
 
@@ -38,22 +41,18 @@ ENDM
 ; returns:			
 ;--------------------------------------
 mDisplayString MACRO string
-
-	; Display a prompt
-
-	; Get user's keyboard input into a memory location
-
-	;
+	PUSH	EDX
+	MOV		EDX, string
+	CALL	WriteString
+	POP		EDX
 
 ENDM
 
 ; Required constants
-LO = 10			; 7 to 12		; 10 default
-HI = 29			; 27 to 32		; 29 default
-ARRAYSIZE = 200 ; 180 to 220	; 200 default
+COUNT =		10		; length of input string can accomodate
+ARRAYSIZE = 10		; Number of valid integers to get from user
 
 .data
-
 prog_title			BYTE	"PROGRAMMING ASSIGNMENT 6: Designing low-level I/O procedures ",13,10,0
 author				BYTE	"Written by: Eva Malpaya ",13,10,0
 ;ec_1				BYTE	"**EC: Program aligns the output columns.",13,10,0	; ec = extra credit
@@ -61,13 +60,17 @@ prompt_intro		BYTE	13,10,"Please provide 10 signed decimal integers. ",13,10
 					BYTE	"Each number needs to be small enough to fit inside a 32 bit register. ",13,10
 					BYTE	"After you have finished inputting the raw numbers I will display ",13,10
 					BYTE	"a list of the integers, their sum, and their average value. ",13,10,13,10,0
-prompt				BYTE	"Please enter a signed number:  ",13,10
-prompt_again		BYTE	"Please try again: ",13,10
-display				BYTE	"You entered the following numbers: ",13,10
-display_sum			BYTE	"The sum of these numbers is: ",13,10
-display_avg			BYTE	"The rounded average is: ",13,10
+prompt				BYTE	"Please enter a signed number:  ",0
+error				BYTE	"ERROR: You did not enter an signed number or your number was too big. ",13,10,0
+prompt_again		BYTE	"Please try again: ",0
+display				BYTE	13,10,"You entered the following numbers: ",13,10,0
+display_sum			BYTE	"The sum of these numbers is: ",13,10,0
+display_avg			BYTE	"The rounded average is: ",13,10,0
 goodbye				BYTE	13,10,"Thanks for playing!  ",0
-count				DWORD	?
+array				DWORD	ARRAYSIZE DUP(?)
+userInput			BYTE	?
+bytesRead			DWORD	0
+isValid				DWORD	1
 
 .code
 main PROC
@@ -77,7 +80,17 @@ main PROC
 	CALL	introduction
 
 	; Get 10 valid integers from the user
-	mGetString	prompt, count
+	PUSH	[isValid]			;44
+	PUSH	OFFSET error		;40
+	PUSH	array				;36
+	PUSH	OFFSET userInput	;32
+	PUSH	bytesRead			;28
+	PUSH	COUNT				;24
+	PUSH	ARRAYSIZE			;20
+	PUSH	OFFSET prompt_intro	;16
+	PUSH	OFFSET prompt		;12
+	PUSH	OFFSET prompt_again	;8
+	CALL	getNumbers
 
 
 	; Store these numeric values in an array
@@ -90,7 +103,7 @@ main PROC
 
 
 	; Display the integers, their sum, and their average
-	mDisplayString, string
+	;mDisplayString display
 
 
 	; Say goodbye
@@ -105,55 +118,200 @@ main ENDP
 ; the program itself, and displays the extra credit print statements 
 ; (if necessary).
 ; preconditions:	intro1, intro2 are strings
-; postconditions:	EDX changed changed but restored
+; postconditions:	
 ; receives:			intro1, intro2
 ; returns:			none
 ;--------------------------------------
 introduction PROC
 	PUSH	EBP
 	MOV		EBP, ESP
-	; preserve registers
-	PUSH	EDX		
+	; preserve registers	
 
-	; Introduce the program title and programmer's name
-	MOV		EDX, [EBP+12]
-	CALL	WriteString
+	; Introduce the program title
+	mDisplayString [EBP+12]
 
-	; Introduce the program
-	MOV		EDX, [EBP+8]
-	CALL	WriteString
+	; Introduce the programmer's name
+	mDisplayString [EBP+8]
 
 	; Display the extra credit print statements 
-	; N/A this project
 
 	; restore registers
-	POP		EDX		
 	POP		EBP
 	RET		8
 introduction ENDP
 
+;--------------------------------------
+; 
+; (if necessary).
+; preconditions:	
+; postconditions:	
+; receives:			
+; returns:			
+;--------------------------------------
+getNumbers PROC
+	PUSH	EBP
+	MOV		EBP, ESP
+	; preserve registers	
+	PUSH	ECX
+
+	; Display the prompt intro
+	mDisplayString [EBP+16]	;prompt_intro
+
+	; Get 10 valid integers from the user.
+	MOV		ECX, [EBP+20]	;ARRAYSIZE
+_fillLoop:
+	PUSH	ECX
+	PUSH	[EBP+44]		;28	;isValid
+	PUSH	[EBP+40]		;24	;error
+	PUSH	[EBP+12]		;20	;prompt
+	PUSH	[EBP+24]		;16	;COUNT
+	PUSH	[EBP+32]		;12	;userInput
+	PUSH	[EBP+28]		;8	;bytesRead
+	CALL	ReadVal
+	POP		ECX
+	LOOP	_fillLoop
+	
+
+	; restore registers
+	POP		ECX
+	POP		EBP
+	RET		40
+getNumbers ENDP
 
 ;--------------------------------------
 ; 
-;
+; (if necessary).
 ; preconditions:	
 ; postconditions:	
-; receives:			userInput (ref)
+; receives:			
 ; returns:			
 ;--------------------------------------
 ReadVal PROC
+	PUSH	EBP
+	MOV		EBP, ESP
+	
+	; preserve registers	
+	PUSH	EAX		
+	PUSH	EBX		
+	PUSH	ECX
+	PUSH	EDX
 
-	; Invoke mGetString to get user input in form of string of digits
+_startLoop:
+	; Invoke myGetString macro to get user input in form of string of digits
+	;prompt, userInput, count, bytesRead
+	mGetString [EBP+20], [EBP+12], [EBP+16], [EBP+8]
 
+	PUSH	[EBP+28]				;16	;isValid
+	PUSH	[EBP+8]					;12	;bytesRead
+	PUSH	[EBP+12]				;8	;userInput
+	CALL	validate
 
-	; Convert string of ascii digits to numeric value rep (SDWORD),
-	; validating user input as valid (no letters, symbols, etc)
+	; if input is invalid, display error message
+	MOV		EAX, isValid
+	CMP		EAX, 0
+	JE		_notifyInvalid
 
+	; if this point is reached, string is valid; jump to end
+	JMP		_stringIsValid
 
-	; Store value in memory variable 
+_notifyInvalid:
+	mDisplayString [EBP+24]			;error
+	MOV		isValid, 1				;reset checker
+	JMP		_startLoop
 
-	RET
+_stringIsValid:
+
+	; restore registers
+	POP		EDX
+	POP		ECX
+	POP		EBX
+	POP		EAX
+	POP		EBP					
+	RET		24
 ReadVal ENDP
+
+
+;--------------------------------------
+; 
+; (if necessary).
+; preconditions:	
+; postconditions:	
+; receives:			
+; returns:			
+;--------------------------------------
+validate PROC
+	; Create local variables
+	LOCAL	index:	DWORD	; placeholder in string
+
+	; Handled by LOCAL dir
+	;PUSH	EBP
+	;MOV		EBP, ESP
+
+	; preserve registers
+	PUSH	EAX		
+	PUSH	EBX		
+	PUSH	ECX
+	PUSH	EDX
+	PUSH	ESI
+
+	; initialize local variables
+	MOV		index, 0
+
+	MOV		ECX, [EBP+12]	; String length into ECX
+	INC		ECX				; Account for null-terminator
+	MOV		ESI, [EBP+8]	; Address of string into EDI
+
+
+_validateLoop:
+	LODSB	; Puts byte in AL
+
+	; check if signed
+	MOV		EAX, index
+	CMP		EAX, 0
+	JE		_checkSign
+	JMP		_checkNumber
+
+_checkSign:
+	; check if plus sign
+	CMP		AL, 43			;+
+	JE		_isValidChar
+
+	; check if negative sign
+	CMP		AL, 45			;-
+	JNE		_invalidChar
+	JMP		_isValidChar
+
+	; check if characters are numbers
+_checkNumber:
+	CMP		AL, 48			;0
+	JL		_invalidChar
+	CMP		AL, 57			;9
+	JG		_invalidChar
+	JMP		_isValidChar
+
+_isValidChar:
+	INC		index
+	STOSB
+	LOOP	_validateLoop
+	JMP		_endOfString
+
+_invalidChar:
+	MOV		EAX, 0
+	MOV		[EBP+16], EAX
+
+	MOV		EAX, [EBP+16]
+	CALL	WriteDec
+
+_endOfString:
+	; restore registers
+	POP		ESI
+	POP		EDX
+	POP		ECX
+	POP		EBX		
+	POP		EAX
+	;POP		EBP			; Handled by LOCAL dir
+	RET		12
+validate ENDP
 
 
 ;--------------------------------------
