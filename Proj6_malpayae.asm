cecef8dd9f36c442b957f5b52e1c3c3582df027b
Eva Malpaya <malpaya.eva@gmail.com>
2021-03-16 22:59:45 -0700

    Started header comments; saving progress

--- Proj6_malpayae.asm
+++ Proj6_malpayae.asm
@@ -1,26 +1,40 @@
 TITLE Project 6 - String Primitives and Macros      (Proj6_malpayae.asm)
 
 ; Author:	Eva Malpaya
-; Last Modified:	3/14/2021
+; Last Modified:	3/16/2021
 ; OSU email address: malpayae@oregonstate.edu
 ; Course number/section:   CS271 Section 401
 ; Project Number:	6                Due Date:	3/14/2021
-; Description: This program prompts the user to enter numbers to be
-; entered into an array, validates the input, then displays the array.
-;
-; Note: 'array' and 'list' may be used interchangeably in my comments
+; Description: This program gets 10 valid integers from the user,
+; stores them in an array, then displays them, their sum, and their
+; average.
 
 INCLUDE Irvine32.inc
 
-;--------------------------------------
-; 
-; preconditions:	
-; postconditions:	
-; receives:			prompt, userInput, count, bytesRead
-; returns:			userInput, bytesRead updated
-;--------------------------------------
-;				[EBP+28], [EBP+12], [EBP+16], [EBP+8]
+;--------------------------------------------------------------
+; Name:	mGetString
+;
+; Displays a prompt, then places user's keyboard input into a memory
+; location.
+;
+; Preconditions: mPrompt is address of a null-terminated string, 
+; mUserInput is address of a string one size bigger than mCount, 
+; mBytesRead is a DWORD
+;
+; Receives:
+; mPrompt		= prompt address
+; mUserInput	= input buffer
+; mCount		= max size user can enter
+; mBytesRead	= characters entered by user
+;
+; Returns:		mUserInput and mBytesRead udpated
+;--------------------------------------------------------------
 mGetString MACRO mPrompt, mUserInput, mCount, mBytesRead
+
+	; preserve registers
+	PUSH	EDX
+	PUSH	ECX
+
 	; Display a prompt
 	mDisplayString mPrompt
 
@@ -31,28 +45,47 @@ mGetString MACRO mPrompt, mUserInput, mCount, mBytesRead
 	MOV		mUserInput, EDX
 	MOV		mBytesRead, EAX
 
-ENDM
-
-
-;--------------------------------------
-; 
-; preconditions:	
-; postconditions:	
-; receives:			string (ref)
-; returns:			
-;--------------------------------------
-mDisplayString MACRO string
-	PUSH	EDX
-	MOV		EDX, string
-	CALL	WriteString
+	; restore registers
+	POP		ECX
 	POP		EDX
 
 ENDM
 
-; Required constants
-COUNT =		32		; length of input string can accomodate
-ARRAYSIZE = 10		; Number of valid integers to get from user
-;ARRAYSIZE = 1		; debug only
+
+;--------------------------------------------------------------
+; Name:	mDisplayString
+;
+; Prints the string store in the specified memory location.
+;
+; Preconditions: mString is a null-terminated string
+;
+; Receives:
+; mString		= string to print
+;
+; Returns:		none; output to terminal only
+;--------------------------------------------------------------
+mDisplayString MACRO mString
+
+	; preserve registers
+	PUSH	EDX
+
+	MOV		EDX, mString
+	CALL	WriteString
+
+	; restore registers
+	POP		EDX
+
+ENDM
+
+;--------------------------------------------------------------
+; Constants
+;
+;
+;--------------------------------------------------------------
+
+COUNT =		32		; length user input string can accomodate
+ARRAYSIZE = 10		; Number of valid numbers to get from the user
+
 
 .data
 prog_title			BYTE	"PROGRAMMING ASSIGNMENT 6: Designing low-level I/O procedures ",13,10,0
@@ -107,29 +140,20 @@ main PROC
 	PUSH	OFFSET prompt_again	;8
 	CALL	testProgram
 
-
-	; Store these numeric values in an array
-
-
 	; Calculate the sum
 	LEA		EDI, sum
-	PUSH	EDI
-	;PUSH	sum					;16
+	PUSH	EDI					;16
 	PUSH	OFFSET array		;12
 	PUSH	ARRAYSIZE			;8
 	CALL	calculateSum
 
-
 	; Calculate the average
 	PUSH	sum					;20
 	LEA		EDI, avg
-	PUSH	EDI
-	;PUSH	avg					;16
+	PUSH	EDI					;16
 	PUSH	OFFSET array		;12
 	PUSH	ARRAYSIZE			;8
 	CALL	calculateAvg
-	
-
 
 	; Display the integers, their sum, and their average
 	;mDisplayString display
@@ -143,7 +167,6 @@ main PROC
 	PUSH	avg					;8
 	CALL	displayResults
 
-
 	; Say goodbye
 	PUSH	OFFSET goodbye
 	CALL	farewell
@@ -151,15 +174,24 @@ main PROC
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
-;--------------------------------------
-; Introduces the program title and programmer's name, introduces
-; the program itself, and displays the extra credit print statements 
-; (if necessary).
-; preconditions:	intro1, intro2 are strings
-; postconditions:	
-; receives:			intro1, intro2
-; returns:			none
-;--------------------------------------
+;--------------------------------------------------------------
+; Name:	introduction
+;
+; Introduces the program title, programmer's name, and any
+; additional extra credit print statements.
+;
+; Preconditions: all inputs are addresses of null-terminated 
+; strings
+;
+; Postconditions: none.
+;
+; Receives:
+; [ebp+16]		= extra credit statement
+; [ebp+12]		= program title
+; [ebp+8]		= programmer's name
+;
+; Returns:		none; output to terminal only
+;--------------------------------------------------------------
 introduction PROC
 	PUSH	EBP
 	MOV		EBP, ESP
@@ -179,14 +211,38 @@ introduction PROC
 	RET		12
 introduction ENDP
 
-;--------------------------------------
-; 
-; (if necessary).
-; preconditions:	
-; postconditions:	
-; receives:			
-; returns:			
-;--------------------------------------
+;--------------------------------------------------------------
+; Name:	testProgram
+;
+; Test program using ReadVal and WriteVal proceures to 
+; get 10 valid integers from the user, store them in an array
+; and display them, their sum, and their integers.
+;
+; Preconditions: strings are addresses to null-terminated strings,
+; [ebp+36] is an SDWORD array of [ebp+24] size, [ebp+44] is an SDWORD,
+; [ebp+28] is a DWORD, [ebp+24] is one less than [ebp+20]
+;
+; Postconditions: none.
+;
+; Receives:
+; [ebp+60]		= subtotal string (extra credit 1)
+; [ebp+56]		= colon string
+; [ebp+52]		= line (number) string (extra  credit 1)
+; [ebp+48]		= space string
+; [ebp+44]		= user's validated SDWORD
+; [ebp+40]		= error message string
+; [ebp+36]		= array to hold user's numbers
+; [ebp+32]		= string to hold user's input
+; [ebp+28]		= characters entered by user
+; [ebp+24]		= max size user can enter
+; [ebp+20]		= size of array for user's numbers
+; [ebp+16]		= prompt intro string
+; [ebp+12]		= prompt string
+; [ebp+8]		= prompt to try again string
+;
+; Returns:		[ebp+26] filled with converted, validated SDWORDs;
+;				[ebp+28] is update with characters entered by user
+;--------------------------------------------------------------
 testProgram PROC
 	; Create local variables
 	LOCAL	lineNumber: DWORD
@@ -198,9 +254,7 @@ testProgram PROC
 
 	; preserve registers	
 	PUSH	EAX		
-	PUSH	EBX		
 	PUSH	ECX
-	PUSH	EDX
 	PUSH	ESI
 	PUSH	EDI
 
@@ -208,55 +262,47 @@ testProgram PROC
 	MOV		lineNumber, 1
 	MOV		subTotalSum, 0
 
-	MOV		ECX, [EBP+20]	; array length into ECX
-	MOV		EDI, [EBP+36]	; Address of array into EDI
+	; Prep array
+	MOV		ECX, [EBP+20]		; array length into ECX
+	MOV		EDI, [EBP+36]		; Address of array into EDI
 
 	; Display the prompt intro
-	mDisplayString [EBP+16]	;prompt_intro
+	mDisplayString [EBP+16]		;prompt_intro
 
 	; Get 10 valid integers from the user.
-	MOV		ECX, [EBP+20]	;ARRAYSIZE
+	MOV		ECX, [EBP+20]		;ARRAYSIZE
+
+	;-------------------------------------
+	; This loop will fill the array with
+	; validated, converted SDWORDS from
+	; the user.
+	;
+	;-------------------------------------
 _fillLoop:
-	;PUSH	ECX
-
-	;PUSH	OFFSET subtotal		;60
-	;PUSH	OFFSET colon		;56
-	;PUSH	OFFSET line			;52
-	;PUSH	OFFSET space		;48	
-
-	;EXTRA CREDIT 1
+	; EXTRA CREDIT 1 - display line number
+	; for valid input only
 	mDisplayString [EBP+52]		;line
 	PUSH	lineNumber
 	CALL	WriteVal
 	mDisplayString [EBP+56]		;colon
 
-	PUSH	[EBP+44]		;32	;numInt
-	PUSH	[EBP+8]			;28	;prompt_again
-	PUSH	[EBP+40]		;24	;error
-	PUSH	[EBP+12]		;20	;prompt
-	PUSH	[EBP+24]		;16	;COUNT
-	PUSH	[EBP+32]		;12	;userInput
-	PUSH	[EBP+28]		;8	;bytesRead
-
+	; Read in a validated input
+	PUSH	[EBP+44]			;32	;numInt
+	PUSH	[EBP+8]				;28	;prompt_again
+	PUSH	[EBP+40]			;24	;error
+	PUSH	[EBP+12]			;20	;prompt
+	PUSH	[EBP+24]			;16	;COUNT
+	PUSH	[EBP+32]			;12	;userInput
+	PUSH	[EBP+28]			;8	;bytesRead
 	CALL	ReadVal
 
-	; debug only
-	;MOV		EDI, [EBP+32]
-	;MOV		EAX, [EDI]
-	;CALL	WriteDec
-	;CALL	CrLf
+	; Move the validated input into the array
+	MOV		ESI, [EBP+44]		; validated input
+	MOV		EAX, [ESI]			
+	MOV		[EDI], EAX			; into array
+	ADD		EDI, 4				; Move to next spot in array to fill
 
-	;POP		ECX
-
-	; move the validated value into the array
-	;MOV		EDI, [EBP+36]	; Address of array into EDI
-	MOV		ESI, [EBP+44]	; address of numInt into ESI
-	MOV		EAX, [ESI]		; numInt into EAX
-	;CALL	WriteInt		; debug only
-	MOV		[EDI], EAX		; EAX into EDI
-	ADD		EDI, 4			; Move into next spot in the array
-
-	; EXTRA CREDIT 1
+	; EXTRA CREDIT 1 - display running subtotal
 	ADD		subTotalSum, EAX
 	mDisplayString [EBP+60]		;subtotal
 	PUSH	subTotalSum
@@ -264,36 +310,51 @@ _fillLoop:
 	CALL	CrLf
 
 	MOV		EAX, 0
-	MOV		[ESI], EAX		; reset numInt to 0
+	MOV		[ESI], EAX			; reset placeholder for user's validated SDWORD
 
 
-	INC		lineNumber		; EXTRA CREDIT 1
+	INC		lineNumber			; EXTRA CREDIT 1 - increment only for valid entries
 	LOOP	_fillLoop
 	
 
 	; restore registers
 	POP		EDI
 	POP		ESI
-	POP		EDX
 	POP		ECX
-	POP		EBX		
 	POP		EAX
-	;POP		EBP
+	;POP		EBP				; Handled by LOCAL dir
 	RET		56
 testProgram ENDP
 
-;--------------------------------------
-; 
-; (if necessary).
-; preconditions:	
-; postconditions:	
-; receives:			
-; returns:			-
-;--------------------------------------
+;--------------------------------------------------------------
+; Name:	ReadVal
+;
+; Invokes the mGetString macro to get user input in form of ascii 
+; digits, validates the user's input, converts the input to SDWORD,
+; then stores it into the provided memory variable.
+;
+; Preconditions: strings are addresses to null-terminated strings,
+; [ebp+32] is an SDWORD, [ebp+8] is a DWORD, [ebp+24] is one less than
+; input buffer
+;
+; Postconditions: none.
+;
+; Receives:
+; [ebp+32]		= user's validated SDWORD
+; [ebp+28]		= prompt to try again string
+; [ebp+24]		= error message string
+; [ebp+20]		= prompt string
+; [ebp+16]		= max size user can enter
+; [ebp+12]		= string to hold user's input
+; [ebp+8]		= characters entered by user
+;
+; Returns:		[ebp+32] filled with a converted, validated SDWORD;
+;				[ebp+8] is update with characters entered by user
+;--------------------------------------------------------------
 ReadVal PROC
 	; Create local variables
 	LOCAL	isValid:	DWORD	; bool for character validation
-	LOCAL	isNegative:	DWORD
+	LOCAL	isNegative:	DWORD	; bool for whether user input is negative
 
 	; Handled by LOCAL dir
 	;PUSH	EBP
@@ -311,16 +372,20 @@ ReadVal PROC
 	MOV		isValid, 1
 	MOV		isNegative, 0
 
+	;-------------------------------------
+	; This loop will repeat until a valid
+	; signed integer is entered by the user.
+	;
+	;-------------------------------------
 _startLoop:
-	; Invoke myGetString macro to get user input in form of string of digits
-	;prompt, userInput, count, bytesRead
+	; Invoke myGetString macro to get user input in form of string of digits.
+	; If the user entered an invalid response in the preceding attempt,
+	; it will prompt with the 'try again' message instead.
 
 	MOV		EAX, isValid
 	CMP		EAX, 0
-	JE		_getStringAgain
+	JE		_getStringAgain		; user entered invalid input in preceding attempt
 	JMP		_getString
-
-
 
 _getStringAgain:
 	mGetString [EBP+28], [EBP+12], [EBP+16], [EBP+8]
@@ -328,85 +393,68 @@ _getStringAgain:
 _getString:
 	mGetString [EBP+20], [EBP+12], [EBP+16], [EBP+8]
 
-	;;;;;;debug
-		; Display a prompt
-	;mDisplayString [EBP+20]
-
-	; Get user's keyboard input into a memory location
-	;MOV		EDX, [EBP+12]
-	;MOV		ECX, [EBP+16]
-	;CALL	ReadString
-	;MOV		[EBP+12], EDX
-	;MOV		[EBP+8], EAX
-	;;;;;;end debug
-
-	; if string is too large, automatically set as invalid
+	; Check input string size -
+	; Too large
 	MOV		EAX, [EBP+8]
 	CMP		EAX, 4
 	JG		_sizeInvalid
-	; if string empty, automatically set as invalid
+	; Too Small - empty string
 	CMP		EAX, 0
 	JE		_sizeInvalid
 
-
 _continueStartLoop:
-	;PUSH	[isValid]				;16	
 	LEA		EDI, isValid
-	PUSH	EDI						;16
+	PUSH	EDI						;16	;isValid
 	PUSH	[EBP+8]					;12	;bytesRead
 	PUSH	[EBP+12]				;8	;userInput
 	CALL	validate
 
-	; if input is invalid, display error message
+	; if the input was invalid, send an error message
 	MOV		EAX, isValid
 	CMP		EAX, 0
 	JE		_notifyInvalid
 
-	; if this point is reached, string is valid
+	; else string is valid
 	JMP		_stringIsValid
 
 _sizeInvalid:
-	MOV		isValid, 0
+	MOV		isValid, 0				; set the bool value if string size was invalid
+
+	; if invalid input given, display the error message
 _notifyInvalid:
 	mDisplayString [EBP+24]			;error
-	;MOV		isValid, 1				;reset checker
-
-	; reset userInput
-	;PUSH	[EBP+8]					;12	;bytesRead
-	;PUSH	[EBP+12]				;8	;userInput
-	;CALL	resetString
 
 	JMP		_startLoop
 
+	;-------------------------------------
+	; Start conversion after user has
+	; entered a validated string of ascii
+	; digits.
+	;
+	;-------------------------------------
 _stringIsValid:
 
-; convert to SDWORD
-	MOV		ECX, [EBP+8]	; String length into ECX
-	;INC		ECX			; Account for null-terminator
-	MOV		ESI, [EBP+12]	; Address of userInput into ESI
+; converting to SDWORD
+							; Prepare:
+	MOV		ECX, [EBP+8]	; length of validated string
+	MOV		ESI, [EBP+12]	; String holding user input
 
-	MOV		EDI, [EBP+32]	; numInt
+	MOV		EDI, [EBP+32]	; SDWORD to hold converted value
 	MOV		EAX, 0
 	MOV		[EDI], EAX
-	;MOV		EAX, 1
-	;MOV		[EDI], EAX
 	
-_convertLoop:				; For numChar in numString
-	LODSB	; Puts byte in AL
+	;-------------------------------------
+	; This loop performs the actual conversion
+	; of the ascii digit string to SDWORD. Formula
+	; is from Module 8, Exploration 1 of course
+	; material (retreived March 2021).
+	;
+	;-------------------------------------
+_convertLoop:				; For length of string
+	LODSB					; Place byte into AL
 
-	; check if signed
-	;PUSH	EAX				; preserve AL before first char check
-
-	;MOV		EAX, ECX
-	;CMP		EAX, 0
-	;JE		_potentialSign
-	;JMP		_isNumber
-
-
-	; check first character if it's not a number
+	; check whether user entered a sign
 _potentialSign:
-	;POP		EAX				; restore AL before first char check
-
 	; check if plus sign
 	CMP		AL, 43			;+
 	JE		_continueConvert
@@ -416,45 +464,36 @@ _potentialSign:
 	JE		_isNeg
 	JMP		_startConvert
 
+	; if it's negative, set the bool checker value
 _isNeg:
 	MOV		isNegative, 1
 	JMP		_continueConvert
 
-	; check if characters are numbers
-_isNumber:
-	POP		EAX				; restore AL before first char check
-
-	; confirmed not trying to convert a sign character at this point1
+	; have reached number portion of string; 
+	; start conversion
 _startConvert:
-	; do something with AL
-	SUB		AL, 48
+	SUB		AL, 48			; subtract 48, per Module formula
 	MOVSX	EBX, AL
 
-	MOV		EAX, [EDI]		; move numInt into EAX
-	IMUL	EAX, 10			; multiply numInt by 10
+	MOV		EAX, [EDI]		; multiply by 10, per Module formula
+	IMUL	EAX, 10			
 
-	ADD		EAX, EBX		; Add these two together to update numInt
-	MOV		[EDI], EAX		; store resulting integer in numInt
+	ADD		EAX, EBX		; Add these two together, per Module formula
+	MOV		[EDI], EAX		; Store it into the SDWORD holder of user input
 _continueConvert:
 	LOOP	_convertLoop	; repeat for length of string
 
+	; check if number is negative to perform additional needed steps
 	CMP		isNegative, 1
 	JE		_makeNeg
 	JMP		_endReadVal
 
+	; if number is negative, perform two's complement negation
+	; so Module formula can work
 _makeNeg:
 	MOV		EAX, [EDI]
 	IMUL	EAX, -1
-	MOV		[EDI], EAX		; store resulting integer in numInt
-
-	;MOV		EAX, [EDI]		; mov numInt into EAX
-	;MOV		ESI, [EBP+12]	; mov userInput into ESI
-	;MOV		[ESI], EAX		; mov numInt into userInput? Why?
-
-	; reset userInput
-	;PUSH	[EBP+8]					;12	;bytesRead
-	;PUSH	[EBP+12]				;8	;userInput
-	;CALL	resetString
+	MOV		[EDI], EAX		
 
 _endReadVal:	
 
