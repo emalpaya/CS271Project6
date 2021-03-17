799c06aa3ec8e858abaa383d3104d7e19719f124
Eva Malpaya <malpaya.eva@gmail.com>
2021-03-16 21:59:21 -0700

    Extra Credit 2 Attempt

--- Proj6_malpayae.asm
+++ Proj6_malpayae.asm
@@ -57,7 +57,8 @@ ARRAYSIZE = 10		; Number of valid integers to get from user
 .data
 prog_title			BYTE	"PROGRAMMING ASSIGNMENT 6: Designing low-level I/O procedures ",13,10,0
 author				BYTE	"Written by: Eva Malpaya ",13,10,0
-ec_1				BYTE	"**EC: Program numbers user input lines & displays running subtotal.",13,10,0	; ec = extra credit
+ec1					BYTE	"**EC: Program numbers user input lines & displays running subtotal. ",13,10,0	; ec = extra credit
+ec2					BYTE	"**EC: Program implements ReadVal and WriteVal for floating point values, using the FPU. ",13,10,0	; ec = extra credit
 prompt_intro		BYTE	13,10,"Please provide 10 signed decimal integers. ",13,10
 					BYTE	"Each number needs to be small enough to fit inside a 32 bit register. ",13,10
 					BYTE	"After you have finished inputting the raw numbers I will display ",13,10
@@ -80,11 +81,23 @@ bytesRead			DWORD	0
 numInt				SDWORD	0	; the string converted to a number
 sum					SDWORD	0
 avg					SDWORD	0
+;;;;;;; EXTRA CREDIT 2 THINGS BELOW
+ec2_title			BYTE	13,10,". . . . . . . . .",13,10
+					BYTE	13,10,"!!!BONUS ROUND!!! ",13,10
+					BYTE	13,10,". . . . . . . . .",13,10
+					BYTE	13,10,"You have reached the EXTRA CREDIT 2 portion of the program. ",13,10
+					BYTE	13,10,"Please provide 10 signed decimal floating point values. ",13,10
+					BYTE	"Each number needs to be small enough to fit inside a 32 bit register. ",13,10
+					BYTE	"After you have finished inputting the raw numbers I will display ",13,10
+					BYTE	"a list of the values, their sum, and their average value. ",13,10,13,10,0
+numFPU				REAL10	0.0
+arrayFPU				REAL10	ARRAYSIZE DUP(?)
 
 .code
 main PROC
 	; Introduce the program
-	PUSH	OFFSET ec_1			;16
+	PUSH	OFFSET ec2			;20
+	PUSH	OFFSET ec1			;16
 	PUSH	OFFSET prog_title	;12
 	PUSH	OFFSET author		;8
 	CALL	introduction
@@ -106,9 +119,6 @@ main PROC
 	PUSH	OFFSET prompt		;12
 	PUSH	OFFSET prompt_again	;8
 	CALL	testProgram
-
-
-	; Store these numeric values in an array
 
 
 	; Calculate the sum
@@ -147,6 +157,61 @@ main PROC
 	; Say goodbye
 	PUSH	OFFSET goodbye
 	CALL	farewell
+	CALL	CrLf
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; EXTRA CREDIT 2 PORTION BELOW
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+		; Introduce the program
+	PUSH	OFFSET ec2_title	;8
+	CALL	ec_introduction
+
+	; Get 10 valid integers from the user
+	LEA		EDI, numFPU	
+	PUSH	EDI					;44
+	PUSH	OFFSET error		;40
+	PUSH	OFFSET arrayFPU		;36
+	PUSH	OFFSET userInput	;32
+	PUSH	bytesRead			;28
+	PUSH	COUNT				;24
+	PUSH	ARRAYSIZE			;20
+	PUSH	OFFSET prompt_intro	;16
+	PUSH	OFFSET prompt		;12
+	PUSH	OFFSET prompt_again	;8
+	CALL	ec_testProgram
+
+
+	; Calculate the sum
+	;LEA		EDI, sum
+	;PUSH	EDI					;16
+	;PUSH	OFFSET array		;12
+	;PUSH	ARRAYSIZE			;8
+	;CALL	ec_calculateSum
+
+
+	; Calculate the average
+	;PUSH	sum					;20
+	;LEA		EDI, avg
+	;PUSH	EDI
+	;PUSH	OFFSET array		;12
+	;PUSH	ARRAYSIZE			;8
+	;CALL	ec_calculateAvg
+	
+
+
+	; Display the integers, their sum, and their average
+	;mDisplayString display
+	;PUSH	OFFSET list_delim	;36
+	;PUSH	OFFSET display		;32
+	;PUSH	OFFSET display_sum	;28
+	;PUSH	OFFSET display_avg	;24
+	;PUSH	OFFSET array		;20
+	;PUSH	ARRAYSIZE			;16
+	;PUSH	sum					;12	
+	;PUSH	avg					;8
+	;CALL	ec_displayResults
+
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
@@ -173,10 +238,11 @@ introduction PROC
 
 	; Display the extra credit print statements 
 	mDisplayString [EBP+16]
+	mDisplayString [EBP+20]
 
 	; restore registers
 	POP		EBP
-	RET		12
+	RET		16
 introduction ENDP
 
 ;--------------------------------------
@@ -1029,6 +1095,488 @@ farewell PROC
 	POP		EBP
 	RET
 farewell ENDP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; EXTRA CREDIT 2 PORTION BELOW
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;--------------------------------------
+; Introduces the program title and programmer's name, introduces
+; the program itself, and displays the extra credit print statements 
+; (if necessary).
+; preconditions:	intro1, intro2 are strings
+; postconditions:	
+; receives:			intro1, intro2
+; returns:			none
+;--------------------------------------
+ec_introduction PROC
+	PUSH	EBP
+	MOV		EBP, ESP
+	; preserve registers	
+
+	; Introduce the new part of the program
+	mDisplayString [EBP+8]
+
+	; restore registers
+	POP		EBP
+	RET		4
+ec_introduction ENDP
+
+;--------------------------------------
+; 
+; (if necessary).
+; preconditions:	
+; postconditions:	
+; receives:			
+; returns:			
+;--------------------------------------
+ec_testProgram PROC
+	PUSH	EBP
+	MOV		EBP, ESP
+
+	; preserve registers	
+	PUSH	EAX		
+	PUSH	EBX		
+	PUSH	ECX
+	PUSH	EDX
+	PUSH	ESI
+	PUSH	EDI
+
+	MOV		ECX, [EBP+20]	; array length into ECX
+	MOV		EDI, [EBP+36]	; Address of array into EDI
+
+	FINIT
+
+	; Display the prompt intro
+	mDisplayString [EBP+16]	;prompt_intro
+
+	; Get 10 valid integers from the user.
+	MOV		ECX, [EBP+20]	;ARRAYSIZE
+_fillLoop:
+	PUSH	[EBP+44]		;32	;numFPU
+	PUSH	[EBP+8]			;28	;prompt_again
+	PUSH	[EBP+40]		;24	;error
+	PUSH	[EBP+12]		;20	;prompt
+	PUSH	[EBP+24]		;16	;COUNT
+	PUSH	[EBP+32]		;12	;userInput
+	PUSH	[EBP+28]		;8	;bytesRead
+
+	CALL	ReadFloatVal
+
+	; move the validated value into the array
+	;MOV		EDI, [EBP+36]	; Address of array into EDI
+	MOV		ESI, [EBP+44]	; address of numInt into ESI
+	MOV		EAX, [ESI]		; numInt into EAX
+	;CALL	WriteInt		; debug only
+	MOV		[EDI], EAX		; EAX into EDI
+	ADD		EDI, 4			; Move into next spot in the array
+
+	MOV		EAX, 0.0
+	MOV		[ESI], EAX		; reset numFPU to 0
+
+	LOOP	_fillLoop
+	
+
+	; restore registers
+	POP		EDI
+	POP		ESI
+	POP		EDX
+	POP		ECX
+	POP		EBX		
+	POP		EAX
+	POP		EBP
+	RET		40
+ec_testProgram ENDP
+
+;--------------------------------------
+; 
+; (if necessary).
+; preconditions:	
+; postconditions:	
+; receives:			
+; returns:			-
+;--------------------------------------
+ReadFloatVal PROC
+	; Create local variables
+	LOCAL	isValidEC:	DWORD	; bool for character validation
+	LOCAL	isNegativeEC:	DWORD
+	LOCAL	leftFloat: REAL10	; left portion of FPU before .
+	LOCAL	rightFloat: REAL10	; Right portion of FPU after .
+
+	; Handled by LOCAL dir
+	;PUSH	EBP
+	;MOV		EBP, ESP
+	
+	; preserve registers	
+	PUSH	EAX		
+	PUSH	EBX		
+	PUSH	ECX
+	PUSH	EDX
+	PUSH	ESI
+	PUSH	EDI
+
+	; initialize local variables
+	MOV		isValidEC, 1
+	MOV		isNegativeEC, 0
+	MOV		leftFloat, 0.0
+	MOV		rightFloat, 0.0
+
+_startLoopEC:
+	; Invoke myGetString macro to get user input in form of string of digits
+	;prompt, userInput, count, bytesRead
+
+	MOV		EAX, isValidEC
+	CMP		EAX, 0
+	JE		_getStringAgainEC
+	JMP		_getStringEC
+
+
+
+_getStringAgainEC:
+	mGetString [EBP+28], [EBP+12], [EBP+16], [EBP+8]
+	JMP		_continueStartLoopEC
+_getStringEC:
+	mGetString [EBP+20], [EBP+12], [EBP+16], [EBP+8]
+
+	;;;;;;debug
+		; Display a prompt
+	;mDisplayString [EBP+20]
+
+	; Get user's keyboard input into a memory location
+	;MOV		EDX, [EBP+12]
+	;MOV		ECX, [EBP+16]
+	;CALL	ReadString
+	;MOV		[EBP+12], EDX
+	;MOV		[EBP+8], EAX
+	;;;;;;end debug
+
+	; if string is too large, automatically set as invalid
+	MOV		EAX, [EBP+8]
+	CMP		EAX, 4
+	JG		_sizeInvalidEC
+	; if string empty, automatically set as invalid
+	CMP		EAX, 0
+	JE		_sizeInvalidEC
+
+
+_continueStartLoopEC:
+	;PUSH	[isValid]				;16	
+	LEA		EDI, isValidEC
+	PUSH	EDI						;16
+	PUSH	[EBP+8]					;12	;bytesRead
+	PUSH	[EBP+12]				;8	;userInput
+	CALL	ec_validate
+
+	; if input is invalid, display error message
+	MOV		EAX, isValidEC
+	CMP		EAX, 0
+	JE		_notifyInvalidEC
+
+	; if this point is reached, string is valid
+	JMP		_stringIsValidEC
+
+_sizeInvalidEC:
+	MOV		isValidEC, 0
+_notifyInvalidEC:
+	mDisplayString [EBP+24]			;error
+	;MOV		isValid, 1				;reset checker
+
+	; reset userInput
+	;PUSH	[EBP+8]					;12	;bytesRead
+	;PUSH	[EBP+12]				;8	;userInput
+	;CALL	resetString
+
+	JMP		_startLoopEC
+
+_stringIsValidEC:
+
+; convert to REAL10
+	MOV		ECX, [EBP+8]	; String length into ECX
+	;INC		ECX			; Account for null-terminator
+	MOV		ESI, [EBP+12]	; Address of userInput into ESI
+
+	
+	LEA		EDI, leftFloat	
+	MOV		EAX, 0.0
+	MOV		[EDI], EAX
+	;MOV		EAX, 1
+	;MOV		[EDI], EAX
+	
+_convertLoopLeftEC:				; For numChar in numString
+	LODSB	; Puts byte in AL
+
+	; check if signed
+	;PUSH	EAX				; preserve AL before first char check
+
+	;MOV		EAX, ECX
+	;CMP		EAX, 0
+	;JE		_potentialSign
+	;JMP		_isNumber
+
+	; check if period
+	CMP		AL, 46			;.
+	JE		_startRight
+
+
+
+	; check first character if it's not a number
+_potentialSignLeftEC:
+	;POP		EAX				; restore AL before first char check
+
+	; check if plus sign
+	CMP		AL, 43			;+
+	JE		_continueConvertLeftEC
+
+	; check if negative sign
+	CMP		AL, 45			;-
+	JE		_isNegLeftEC
+	JMP		_startConvertLeftEC
+
+_isNegLeftEC:
+	MOV		isNegativeEC, 1
+	JMP		_continueConvertLeftEC
+
+	; check if characters are numbers
+_isNumberLeftEC:
+	POP		EAX				; restore AL before first char check
+
+	; confirmed not trying to convert a sign character at this point1
+_startConvertLeftEC:
+	; do something with AL
+	FLD		[EDI]
+	FLD		48.0
+	FSUB
+	FSTP	[EDI]
+
+	FLD		[EDI]
+	FLD		10.0
+	FMUL	
+	FSTP	[EDI]
+
+	ADD		EAX, EBX		; Add these two together to update numInt
+	MOV		[EDI], EAX		; store resulting integer in numInt
+_continueConvertLeftEC:
+	LOOP	_convertLoopEC	; repeat for length of string
+
+	CMP		isNegativeEC, 1
+	JE		_makeNegLeftEC
+	JMP		_endReadValLeftEC
+
+_makeNegLeftEC:
+	MOV		EAX, [EDI]
+	IMUL	EAX, -1
+	MOV		[EDI], EAX		; store resulting integer in numInt
+
+	;MOV		EAX, [EDI]		; mov numInt into EAX
+	;MOV		ESI, [EBP+12]	; mov userInput into ESI
+	;MOV		[ESI], EAX		; mov numInt into userInput? Why?
+
+	; reset userInput
+	;PUSH	[EBP+8]					;12	;bytesRead
+	;PUSH	[EBP+12]				;8	;userInput
+	;CALL	resetString
+
+_endReadValLeftEC:	
+
+
+_startRight:
+	MOV		EDI, [EBP+32]	; numFPU
+	MOV		EAX, 0.0
+	MOV		[EDI], EAX
+	;MOV		EAX, 1
+	;MOV		[EDI], EAX
+
+_convertLoopRightEC:				; For numChar in numString
+	LODSB	; Puts byte in AL
+
+	; check if signed
+	;PUSH	EAX				; preserve AL before first char check
+
+	;MOV		EAX, ECX
+	;CMP		EAX, 0
+	;JE		_potentialSign
+	;JMP		_isNumber
+
+	; check if period
+	CMP		AL, 46			;.
+
+
+
+	; check first character if it's not a number
+_potentialSignRightEC:
+	;POP		EAX				; restore AL before first char check
+
+	; check if plus sign
+	CMP		AL, 43			;+
+	JE		_continueConvertRightEC
+
+	; check if negative sign
+	CMP		AL, 45			;-
+	JE		_isNegRightEC
+	JMP		_startConvertRightEC
+
+_isNegRightEC:
+	MOV		isNegativeEC, 1
+	JMP		_continueConvertRightEC
+
+	; check if characters are numbers
+_isNumberRightEC:
+	POP		EAX				; restore AL before first char check
+
+	; confirmed not trying to convert a sign character at this point1
+_startConvertRightEC:
+	; do something with AL
+	SUB		AL, 48
+	MOVSX	EBX, AL
+
+	MOV		EAX, [EDI]		; move numInt into EAX
+	IMUL	EAX, 10			; multiply numInt by 10
+
+	ADD		EAX, EBX		; Add these two together to update numInt
+	MOV		[EDI], EAX		; store resulting integer in numInt
+_continueConvertRightEC:
+	LOOP	_convertLoopEC	; repeat for length of string
+
+	CMP		isNegativeEC, 1
+	JE		_makeNegRightEC
+	JMP		_endReadValRightEC
+
+_makeNegRightEC:
+	MOV		EAX, [EDI]
+	IMUL	EAX, -1
+	MOV		[EDI], EAX		; store resulting integer in numInt
+
+	;MOV		EAX, [EDI]		; mov numInt into EAX
+	;MOV		ESI, [EBP+12]	; mov userInput into ESI
+	;MOV		[ESI], EAX		; mov numInt into userInput? Why?
+
+	; reset userInput
+	;PUSH	[EBP+8]					;12	;bytesRead
+	;PUSH	[EBP+12]				;8	;userInput
+	;CALL	resetString
+
+_endReadValRightEC:	
+
+	; restore registers
+	POP		EDI
+	POP		ESI
+	POP		EDX
+	POP		ECX
+	POP		EBX		
+	POP		EAX
+	;POP		EBP					; Handled by LOCAL dir
+	RET		28
+ReadFloatVal ENDP
+
+;--------------------------------------
+; 
+; (if necessary).
+; preconditions:	
+; postconditions:	
+; receives:			
+; returns:			
+;--------------------------------------
+ec_validate PROC
+	; Create local variables
+	LOCAL	index:	DWORD	; placeholder in string
+
+	; Handled by LOCAL dir
+	;PUSH	EBP
+	;MOV		EBP, ESP
+
+	; preserve registers
+	PUSH	EAX		
+	PUSH	EBX		
+	PUSH	ECX
+	PUSH	EDX
+	PUSH	ESI
+	PUSH	EDI
+
+	; initialize local variables
+	MOV		index, 0
+
+	MOV		ECX, [EBP+12]	; String length into ECX
+	INC		ECX				; Account for null-terminator
+	MOV		ESI, [EBP+8]	; Address of string into ESI
+
+	MOV		EDI, [EBP+16]	; Reset bool value
+	MOV		EAX, 1
+	MOV		[EDI], EAX
+
+
+_validateLoopEC:	
+	LODSB	; Puts byte in AL
+
+	; check if signed
+	PUSH	EAX				; preserve AL before first char check
+
+	MOV		EAX, index
+	CMP		EAX, 0
+	JE		_checkSignEC
+	JMP		_checkNumberEC
+
+
+	; check first character if it's not a number
+_checkSignEC:
+	POP		EAX				; restore AL before first char check
+
+	; check if plus sign
+	CMP		AL, 43			;+
+	JE		_isValidCharEC
+
+	; check if negative sign
+	CMP		AL, 45			;-
+	JE		_isValidCharEC
+	JMP		_continueCheckEC
+
+	; check if characters are numbers
+_checkNumberEC:
+	POP		EAX				; restore AL before first char check
+
+_continueCheckEC:
+	; check if end of string
+	CMP		AL, 0
+	JE		_endOfStringEC
+
+	CMP		AL, 48			;0
+	JL		_invalidCharEC
+	CMP		AL, 57			;9
+	JG		_invalidCharEC
+	CMP		AL, 46			;.
+	JNE		_invalidCharEC
+	JMP		_isValidCharEC
+
+_isValidCharEC:
+	INC		index
+	STOSB
+	LOOP	_validateLoopEC
+	JMP		_endOfStringEC
+
+_invalidCharEC:
+	MOV		EDI, [EBP+16]
+	MOV		EAX, 0
+	MOV		[EDI], EAX
+
+	MOV		EDX, [EBP+16]
+	;CALL	WriteDec	; debug only
+
+	;LEA		EAX, [EBP+16]
+	;MOV		EAX, 0
+
+	;MOV		EAX, [EBP+16]
+	;MOV		EAX, [EDI]
+	;CALL	WriteDec
+
+_endOfStringEC:
+	; restore registers
+	POP		EDI
+	POP		ESI
+	POP		EDX
+	POP		ECX
+	POP		EBX		
+	POP		EAX
+	;POP		EBP			; Handled by LOCAL dir
+	RET		12
+ec_validate ENDP
 
 
 END main
